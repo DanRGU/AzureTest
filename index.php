d724cbead93ac10ce287ba1aa84e533e3f54e9cf
1308353 <1308353@rgu.ac.uk>
2015-10-19 15:16:01 +0100

    Added

--- index.php
+++ index.php
@@ -7,7 +7,18 @@
         <body>
             <p>
                 <?php
-                    echo "Hello World";
+
+                   $myage = 17;
+
+                if($myage > 16){
+                    print "can buy specs";
+                }
+                else if($myage > 18){
+                    print "can buy mugs";
+                }
+                else if($myage > 21){
+                    print "can buy sausage rolls";
+                }
                 ?>
             </p>
         </body>
