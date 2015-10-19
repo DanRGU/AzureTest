01694ece5c981aaa52083866af5424bf0cd1a44e
1308353 <1308353@rgu.ac.uk>
2015-10-19 15:41:33 +0100

    Added

--- index.php
+++ index.php
@@ -8,21 +8,17 @@
             <p>
                 <?php
 
+                for ($i = 1; $i < 30; $i++) {
+                    if ($i % 2) {
+                        print "On the " + $i + "of the month Specs are available";
+                    } else if ($i % 3) {
+                        print "On the " + $i + "of the month Mugs are available";
 
+                    } else if ($i % 4) {
+                        print "On the " + $i + "of the month SGROLLSSSSSSSSSS are available";
 
-                    $provisionedActivities = array("specs", "drugs", "rock and roll");
-
-                    foreach($provisionedActivities as $x){
-                        print "<p>$x</p>";
                     }
-
-                $provisionedActivities[1] = "hugs";
-                unset($provisionedActivities[2]);
-                foreach($provisionedActivities as $x){
-                    print "<p>$x</p>";
                 }
-
-
 
 
                 ?>
