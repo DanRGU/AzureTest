8922fb571633887a68c1aefe4d7b75d36823f9db
1308353 <1308353@rgu.ac.uk>
2015-10-19 16:11:39 +0100

    Added

--- index.php
+++ index.php
@@ -13,22 +13,22 @@
                 $i = 1;
 
 
-                while($count1 >= 0 || $count2 >= 0 || $count3 >= 0){
+                while($count1 >= 0 or $count2 >= 0 or $count3 >= 0){
                 $i++;
                 $todaysGood = rand(0,2);
                     echo $count1 . $count2 . $count3;
 
                 if ($todaysGood == 0) {
                     $count1--;
-                    print "<p>On the " . $i . "of the month Specs are available</p>";
+                    print "<p>On the " . $i . " of the month Specs are available</p>";
 
                 } else if ($todaysGood == 1) {
                     $count2--;
-                    print "<p>On the " . $i . "of the month Mugs are available</p>";
+                    print "<p>On the " . $i . " of the month Mugs are available</p>";
 
                 } else if ($todaysGood == 2) {
                     $count3--;
-                    print "<p>On the " . $i . "of the month SGROLLSSSSSSSSSS are available.</p>";
+                    print "<p>On the " . $i . " of the month SGROLLSSSSSSSSSS are available.</p>";
 
                 }
                 }
