de8f0e0caa1d7671d5b447f1e2be6e432b888715
1308353 <1308353@rgu.ac.uk>
2015-10-19 15:56:59 +0100

    Added

--- index.php
+++ index.php
@@ -7,18 +7,31 @@
         <body>
             <p>
                 <?php
+                $count1 = 7;
+                $count2 = 7;
+                $count3 = 7;
+                $i = 1;
 
-                for ($i = 1; $i < 31; $i++) {
-                    if ($i % 2) {
-                        print "On the " . $i . "of the month Specs are available";
-                    } else if ($i % 3) {
-                        print "On the " . $i . "of the month Mugs are available";
 
-                    } else if ($i % 4) {
-                        print "On the " . $i . "of the month SGROLLSSSSSSSSSS are available";
+                while($count1 > 0 || $count2 > 0 || $count3 > 0){
+                $i++;
+                $todaysGood = rand(0,2);
 
-                    }
                 }
+                if ($todaysGood == 1) {
+                    $count1--;
+                    print "On the " . $i . "of the month Specs are available";
+
+                } else if ($todaysGood == 2) {
+                    $count2--;
+                    print "On the " . $i . "of the month Mugs are available";
+
+                } else if ($todaysGood == 3) {
+                    $count3--;
+                    print "On the " . $i . "of the month SGROLLSSSSSSSSSS are available";
+
+                }
+                print "No more goods are available this month"
 
 
                 ?>
