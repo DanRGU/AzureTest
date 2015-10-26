fde6aabacd6d6a18e8da7dbfe5bb4165b5655360
1308353 <1308353@rgu.ac.uk>
2015-10-26 15:38:50 +0000

    Added

--- index.php
+++ index.php
@@ -10,6 +10,20 @@
                 <a href="helloSelector.php">"Planet Selector"</a>
                 <a href="form.php">"Form"</a>
 
+                <?php
+
+                    $db = new mysqli(
+                        "br-cdbr-azure-south-a.cloudapp.net",
+                        "b3d04eaaa95e7f",
+                        "6b2faa59",
+                        "DanRGUDatabase"
+                    );
+                if($db->connect_errno){
+                    die('Connectfailed['.$db->connect_error.']');
+                }
+
+                ?>
+
             </p>
         </body>
 
