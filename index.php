1779430f9f859ee60d33bcf1f9491ba7c3ce3b18
1308353 <1308353@rgu.ac.uk>
2015-10-26 15:45:46 +0000

    Added

--- index.php
+++ index.php
@@ -15,13 +15,13 @@
                     $db = new mysqli(
                         "br-cdbr-azure-south-a.cloudapp.net",
                         "b3d04eaaa95e7f",
-                        "6b2faa5",
+                        "6b2faa59",
                         "DanRGUDatabase"
                     );
                 if($db->connect_errno){
                     die('Connectfailed['.$db->connect_error.']');
                 }
-                else echo "Hello";
+                else echo "Connection to database established.  Mad HAXXZORZ initiated.";
                 ?>
 
             </p>
