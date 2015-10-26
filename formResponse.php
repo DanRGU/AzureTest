3167860d8a3265e1bcd3caf73752359d32f8ba74
1308353 <1308353@rgu.ac.uk>
2015-10-26 15:15:53 +0000

    Added

--- formResponse.php
+++ formResponse.php
@@ -10,13 +10,13 @@
 
 <?php
 
-        $Forename = $_POST["forename"];
+        $forename = $_POST["forename"];
         $surname = $_POST["surname"];
         $dob = $_POST["dob"];
         $gender = $_POST["gender"];
         $superpowers = $_POST["superpowers"];
 
-        echo $Forename . $surname . $dob . $superpowers . $gender;
+        echo $forename . $surname . $dob . $superpowers . $gender;
 ?>
 
 
