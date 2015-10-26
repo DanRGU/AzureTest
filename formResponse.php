d46b55bc220f48fbb7164e822270b9bd0a12decc
1308353 <1308353@rgu.ac.uk>
2015-10-26 14:55:20 +0000

    Added

--- formResponse.php
+++ formResponse.php
@@ -6,20 +6,6 @@
 </head>
 <body>
 <p>
-    <?php
-
-        $forename = $_POST["forename"];
-        $surname = $_POST["surname"];
-        $dob = $_POST["dob"];
-        $superpowers = $_POST["superpowers"];
-        $gender = $_POST["gender"];
-
-        echo $forename . $surname;
-?>
-
-
-
-    ?>
 
     <form action="formResponse.php" method="post">
         <label>Forename</label><input type=“text” name=“forename”>
@@ -29,6 +15,18 @@
         <label>Superpowers</label><input type=“text” name=“superpowers”>
         <input type="submit" value="Submit">
     </form>
+
+<?php
+
+        $forename = $_POST["forename"];
+        $surname = $_POST["surname"];
+        $dob = $_POST["dob"];
+        $superpowers = $_POST["superpowers"];
+        $gender = $_POST["gender"];
+
+        echo $forename . $surname . $dob . $superpowers . $gender;
+?>
+
 
 
 </p>
