6ee60c0670be2d2b970d3c53c3cd0231ce74ee2d
1308353 <1308353@rgu.ac.uk>
2015-10-26 15:44:18 +0000

    Added

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                 if($db->connect_errno){
                     die('Connectfailed['.$db->connect_error.']');
                 }
-
+                else echo "Hello";
                 ?>
 
             </p>
