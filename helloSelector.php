132a210007896e72be1f4cb096a0ed4233449830
1308353 <1308353@rgu.ac.uk>
2015-10-26 14:41:39 +0000

    Added

--- helloSelector.php
+++ helloSelector.php
@@ -12,9 +12,9 @@
 
     ?>
 
-    <a href="http://danrgu.azurewebsites.net/helloPrinter.php?planet=earth">"Earth"</a>
-    <a href="http://danrgu.azurewebsites.net/helloPrinter.php?planet=Mars">"Mars"</a>
-    <a href="http://danrgu.azurewebsites.net/helloPrinter.php?planet=Uranus">"Uranus"</a>
+    <a href="http://danrgu.azurewebsites.net/helloPrinter.php?planet=Earth">Earth</a>
+    <a href="http://danrgu.azurewebsites.net/helloPrinter.php?planet=Mars">Mars</a>
+    <a href="http://danrgu.azurewebsites.net/helloPrinter.php?planet=Uranus">Uranus</a>
 
 </p>
 </body>
