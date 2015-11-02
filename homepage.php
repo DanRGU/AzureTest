4999dd19bb656ee032038263b83544d12e9c120a
1308353 <1308353@rgu.ac.uk>
2015-11-02 15:12:09 +0000

    Added

--- homepage.php
+++ homepage.php
@@ -13,19 +13,26 @@
     echo "<p>SESSIONS</p>";
     $username = "Dan";
     $password = "pass";
+    $login = FALSE;
 
     if($username == $_POST["username"] && $password == $_POST["password"]){
 
         echo "Username and password correct";
         $_SESSION['username'] = $_POST['username'];
         $_SESSION['access_level'] = "standarduser";
+        $login = TRUE;
 
     }
     else {
         echo "<p>Username or Password incorrect.</p>";
     }
 
-    echo  "<p>Username is " . $_SESSION['username'] . "</p>";
+    if($login) {
+        echo "<p>Username is " . $_SESSION['username'] . "</p>";
+    }
+    else{
+        echo "<p>No Username yet. " . "</p>";
+    }
 
     $accessLevel = $_SESSION[access_level];
 
