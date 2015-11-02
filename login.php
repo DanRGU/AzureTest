df6c4021c8bf8a203092832e6fc73394a853aa21
1308353 <1308353@rgu.ac.uk>
2015-11-02 14:05:53 +0000

    Added

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 <p>
-<form action="loginCheck.php" method="post">
+<form action="homepage.php" method="post">
 
     <label>Username</label><input type="text" name="username">
     <label>Password</label><input type="text" name="password">
