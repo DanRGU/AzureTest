a50eeca98fc7e78869509ddf7e0931328f5016cd
1308353 <1308353@rgu.ac.uk>
2015-11-02 14:05:08 +0000

    Added

---
+++ homepage.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+<body>
+<p>
+
+    <?php
+    $username = "Dan";
+    $password = "pass";
+    # echo $_POST["username"];
+
+    if($username == $_POST["username"] && $password == $_POST["password"]){
+
+        setcookie('username', 'Dan');
+        setcookie('access_level','standarduser');
+        echo "Username and password correct";
+    }
+    else {
+        echo "NO";
+    }
+
+    echo $_COOKIE['username'];
+    $accessLevel = $_COOKIE[access_level];
+
+    displayAccessLevelInformation($accessLevel);
+
+    function displayAccessLevelInformation($access_level){
+        if ($access_level == "standarduser") {
+            echo "<p> You are currently logged in as a standard user</p>";
+
+        }
+        elseif ($access_level == "root"){
+            echo "<p> You are currently logged in as a root user</p>";
+            echo "<p> You now have access to additional administrative features</p>";
+
+        }
+
+    }
+
+
+    ?>
+
+
+
+</p>
+</body>
+
+</html>
