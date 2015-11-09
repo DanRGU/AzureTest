7a9ca2704c1500d7264d118b0c99ae5bce0f8c2a
1308353 <1308353@rgu.ac.uk>
2015-11-09 15:12:08 +0000

    Added

--- wiki.php
+++ wiki.php
@@ -28,23 +28,20 @@
                             <span class="icon-bar"></span>
                         </button>
                         <a class="navbar-brand" href="#">Wiki</a>
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Action <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </div>
                     </div>
 
-                    <!-- Collect the nav links, forms, and other content for toggling -->
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">Something else here</a></li>
-                                    <li><a href="#">Separated link</a></li>
-                                    <li><a href="#">One more separated link</a></li>
-                                </ul>
-                            </li>
-                        </ul>
 
                     </div><!-- /.navbar-collapse -->
                 </div><!-- /.container-fluid -->
@@ -62,7 +59,7 @@
             if($db->connect_errno){
                 die('Connectfailed['.$db->connect_error.']');
             }
-            else echo "<p> Connection to database established.</p>";
+           # else echo "<p> Connection to database established.</p>";
 
             $sql_query = "SELECT * FROM topics";
             $result = $db->query($sql_query);
