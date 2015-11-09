e1a97682625c144bf3ea321c8906e740cb5ffc0b
1308353 <1308353@rgu.ac.uk>
2015-11-09 10:09:11 +0000

    Added

--- index.php
+++ index.php
@@ -8,7 +8,13 @@
             <p>
 
 
-                <a href="login.php"><p>"Form"</p></a>
+                <form action="homepage.php" method="post">
+
+                    <label>Username</label><input type="text" name="username">
+                    <label>Password</label><input type="password" name="password">
+                    <button type="submit" onclick=  value="Submit">Log In</button>
+
+                </form>
 
                 <?php
 
