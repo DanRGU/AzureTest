327cae940059539a0de99700fa73ad0c971f4bc4
1308353 <1308353@rgu.ac.uk>
2015-11-09 15:15:09 +0000

    Added

--- wiki.php
+++ wiki.php
@@ -28,22 +28,21 @@
                             <span class="icon-bar"></span>
                         </button>
                         <a class="navbar-brand" href="#">Wiki</a>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Action <span class="caret"></span>
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a></li>
-                            </ul>
-                        </div>
                     </div>
 
+                    <div class="dropdown">
+                        <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            Dropdown
+                            <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </div>
 
-                    </div><!-- /.navbar-collapse -->
                 </div><!-- /.container-fluid -->
             </nav>
 
