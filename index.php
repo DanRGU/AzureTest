1d00e728e2e8194a5675acc28f1191eb32252989
1308353 <1308353@rgu.ac.uk>
2016-01-16 17:39:54 +0000

    Added

--- index.php
+++ index.php
@@ -8,40 +8,11 @@
             <p>
 
 
-                <form action="homepage.php" method="post">
-
-                    <label>Username</label><input type="text" name="username">
-                    <label>Password</label><input type="password" name="password">
-                    <button type="submit" onclick=  value="Submit">Log In</button>
-
-                </form>
-
                 <?php
-
-                                    $db = new mysqli(
-                    "br-cdbr-azure-south-a.cloudapp.net",
-                    "b3d04eaaa95e7f",
-                    "6b2faa59",
-                    "DanRGUDatabase"
-                );
-
-                if($db->connect_errno){
-                    die('Connectfailed['.$db->connect_error.']');
-                }
-                else echo "<p> Connection to database established.</p>";
-
-                $sql_query = "SELECT * FROM articletext";
-
-                $result = $db->query($sql_query);
-                while($row = $result->fetch_array()){
-                    echo "<p>" . $row['text'] . "</p>";
-                }
-
-                $result->close();
-                $db->close();
-
-
-                ?>
+                $juice = 'apple';
+                echo "I drank some $juice juice<br />";
+                echo 'I drank some $juice juice<br />';
+ ?>
 
             </p>
         </body>
