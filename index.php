1796537fc3b21f5b432288aea0aa7abb89fbadc6
1308353 <1308353@rgu.ac.uk>
2016-01-16 18:08:42 +0000

    Added

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
              $arr = array('one','two','three', 'four','stop','five');
                 while(list(, $val)=each($arr)){
                     if($val=='stop'){
-                        break 1;
+                        break;
                     }
                     echo "$val<br />\n";
                 }
