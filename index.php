e4a734c13645047a77e52b3716344bfe0ac7ef58
1308353 <1308353@rgu.ac.uk>
2016-01-16 18:30:11 +0000

    Added

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
              $arr = array('Monday','Tesuday','Wednesday', 'Thursday','Friday');
 
                 for($k = 0; $k < 5; $k++){
-                    echo $array[$k];
+                    echo $arr[$k];
                 }
 
                 function newFunction($input){
                     for($i = 0; $i < 5; $i++){
-                        $array[$i] = substr($input[$i], 0, 3);
+                        $input[$i] = substr($input[$i], 0, 3);
                     }
                 }
 
@@ -26,7 +26,7 @@
                 newFunction($arr);
 
                 for($j = 0; $j < 5; $j++){
-                    echo $array[$j];
+                    echo $arr[$j];
                 }
 
  ?>
