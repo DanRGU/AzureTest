9e3da0f3697fb5258d46cda2f5f1bddf5a9264f0
1308353 <1308353@rgu.ac.uk>
2016-01-17 11:14:45 +0000

    Added

--- index.php
+++ index.php
@@ -8,28 +8,22 @@
             <p>
 
 
+
                 <?php
-             $arr = array('Monday','Tesuday','Wednesday', 'Thursday','Friday');
 
-                for($k = 0; $k < 5; $k++){
-                    echo $arr[$k];
+                function incr_a($inc)
+                {
+                    $a = 0;
+                    $a = $a + $inc;
+                    return $a;
                 }
+                
+                   $a = 0;
+                   incr_a(10);
+                   echo $a;
 
-                function newFunction($input){
-                    for($i = 0; $i < 5; $i++){
-                        $input[$i] = substr($input[$i], 0, 3);
-                    }
-                }
 
-                echo " ";
-
-                newFunction($arr);
-
-                for($j = 0; $j < 5; $j++){
-                    echo $arr[$j];
-                }
-
- ?>
+                ?>
 
             </p>
         </body>
